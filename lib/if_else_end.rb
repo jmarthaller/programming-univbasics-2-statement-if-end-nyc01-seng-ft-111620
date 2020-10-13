cf877aaea44c76b04149a66e0c1d914623a79916
Jacob Marthaller <marthaller.jr@gmail.com>
2020-10-13 17:46:10 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,14 +1,16 @@
 # Write your solution here
-rain_chance = -1000
+# rain_chance = -1000
 
 
-if rain_chance <= 0.25
-  puts "There should be sun!"
-elsif rain_chance > 0.25 && rain_chance < 0.75
-  puts "Pack an umbrella!"
-else
-  puts "Let's just stay home"
-end
+# if rain_chance <= 0.25
+#   puts "There should be sun!"
+# elsif rain_chance > 0.25 && rain_chance < 0.75
+#   puts "Pack an umbrella!"
+# else
+#   puts "Let's just stay home"
+# end
 
 
-puts 
+puts "Do you know what year it is"
+this_year = 2020
+puts "It is 2020" if this_year == 2020
