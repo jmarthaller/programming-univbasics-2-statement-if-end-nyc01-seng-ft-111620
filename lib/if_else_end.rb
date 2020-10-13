779e2251f1ae60273c907a2f6c698ba7488baf7c
Jacob Marthaller <marthaller.jr@gmail.com>
2020-10-13 17:52:20 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -3,11 +3,9 @@
 
 
 if Time.now.to_i $ 2 == 0
-  puts "There should be sun!"
-elsif rain_chance > 0.25 && rain_chance < 0.75
-  puts "Pack an umbrella!"
+  puts "Even!"
 else
-  puts "Let's just stay home"
+  puts "Odd!"
 end
 
 
