1de6b7fbbc91d47bb18592a96c36e7527fa997d8
Jacob Marthaller <marthaller.jr@gmail.com>
2020-10-13 17:40:43 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,7 @@
 # Write your solution here
 run_this_code = false
 puts "code before if...end"
-if run_this_code
-  puts "code inside"
+if !run_this_code
+  puts "aha still got it"
 end
 puts "Code after if...end"
