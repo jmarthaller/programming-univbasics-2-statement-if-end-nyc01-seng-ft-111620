1d613690aac174efaf122a131a1eb2639df3a3a7
Jacob Marthaller <marthaller.jr@gmail.com>
2020-10-13 17:56:47 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,6 @@
 current_time = Time.now.to_i
 
-if current_time.even?    #% 2 == 0
+if current_time % 2 == 0 # can also append .even? to current_time to get same functionality
   puts "Even!"
 else
   puts "Odd!"
